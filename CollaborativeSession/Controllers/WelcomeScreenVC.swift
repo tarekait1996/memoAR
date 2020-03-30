e09be7555b44a5946aa47192523e5c9ee48b6970
Tarek Ait Hamouda <tarekaithamouda@gmail.com>
2020-03-30 14:24:13 -0400

    added new VC for initialScreen

---
+++ CollaborativeSession/Controllers/WelcomeScreenVC.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeScreenVC.swift
+//  MemoAR
+//
+//  Created by tarek ait hamouda on 2020-03-30.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+class WelcomeScreenVC: UIViewController {
+    
+    @IBOutlet weak var startBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
