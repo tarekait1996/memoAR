115289f6a7f7a38bca4dcab66218f4a1eabb4e1b
narra <narra_pangan@hotmail.com>
2020-03-30 22:54:11 -0400

    Temporary changes to test swiping

---
+++ CollaborativeSession/Controllers/MenuBarController.swift
@@ -0,0 +1,9 @@
+//
+//  MenuBarController.swift
+//  MemoAR
+//
+//  Created by Narra Pangan on 2020-03-30.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import Foundation
---
+++ CollaborativeSession/Controllers/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  MemoAR
+//
+//  Created by Narra Pangan on 2020-03-30.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout{
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+
+}
+
+
+
